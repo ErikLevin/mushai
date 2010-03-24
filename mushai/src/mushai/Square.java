24808063b001a4b48c8a281493588ece61807385
darukusu <darukusu@850ccf3f-6c78-129b-0b64-280ab6fbb7b6>
2010-03-24 09:02:08 +0000

    nooow

--- mushai/src/mushai/Square.java
+++ mushai/src/mushai/Square.java
@@ -5,7 +5,7 @@
 
 package mushai;
 import java.util.ArrayList;
-import javax.swing.Icon;
+import java.awt.Point;
 
 /**
  *
@@ -13,8 +13,37 @@ import javax.swing.Icon;
  */
 public class Square extends Piece {
 
-    ArrayList<Tile> legalMoves;
     public Square() {
         super("hej");
+    }
+    public ArrayList<Point> getSquareMoves(){
+        ArrayList<Point> moves = new ArrayList<Point>();
+        Point forward,backward,left,right;
+        int x = (int)position.getX();
+        int y = (int)position.getY();
+        if (Color.RED){
+            forward = new Point(x,y-1);
+            backward = new Point(x,y+1);
+            left = new Point(x-1,y);
+            right = new Point(x+1,y);
+            
+        }
+        else if (Color.BLACK){
+            forward = new Point(x,y+1);
+            backward = new Point(x,y-1);
+            left = new Point(x+1,y);
+            right = new Point(x-1,y);
+            moves.add(forward);
+            moves.add(backward);
+            moves.add(left);
+            moves.add(right);
+        }else{
+            return null;
+        }
+        moves.add(forward);
+        moves.add(backward);
+        moves.add(left);
+        moves.add(right);
+        return moves;
     }
 }
