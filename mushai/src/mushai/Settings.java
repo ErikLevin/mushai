774bf08e11d40b653616ad4ec9bb926c1aba2580
ErikAxelsson1 <ErikAxelsson1@850ccf3f-6c78-129b-0b64-280ab6fbb7b6>
2010-03-24 08:23:57 +0000

    adding Settings

---
+++ mushai/src/mushai/Settings.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mushai;
+
+/**
+ *
+ * @author bark
+ */
+public class Settings {
+private static int playboardSize=4;
+
+    public static int getPlayboardSize(){
+        return playboardSize;
+    }
+
+}
