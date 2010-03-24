3e775a9871fbf1efd5b7f950cf9d458c23db7d31
ErikAxelsson1 <ErikAxelsson1@850ccf3f-6c78-129b-0b64-280ab6fbb7b6>
2010-03-24 08:23:57 +0000

    adding Settings

--- trunk/mushai/src/mushai/Playboard.java
+++ trunk/mushai/src/mushai/Playboard.java
@@ -7,9 +7,6 @@ package mushai;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -18,14 +15,12 @@ import javax.swing.JPanel;
  */
 
 public class Playboard extends JPanel {
-int playboardSize=4;
     public Playboard(){
         super();
-
+        int playboardSize=Settings.getPlayboardSize();
         this.setPreferredSize(new Dimension(200,200));
 
         setLayout(new GridLayout(playboardSize,playboardSize));
-        //add(new JLabel("ololll"));
         for(int i=0;i<playboardSize*playboardSize;i++){
             add(new Square());
         }
