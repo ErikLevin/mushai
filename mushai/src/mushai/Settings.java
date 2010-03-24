c77986b6d2dd50ef7edd29b7cbfdc175694453bb
ErikAxelsson1 <ErikAxelsson1@850ccf3f-6c78-129b-0b64-280ab6fbb7b6>
2010-03-24 09:00:24 +0000

    add pices

--- mushai/src/mushai/Settings.java
+++ mushai/src/mushai/Settings.java
@@ -5,12 +5,15 @@
 
 package mushai;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author bark
  */
 public class Settings {
 private static int playboardSize=4;
+private static ArrayList<Player> ar= new ArrayList<Player>();
 
     public static int getPlayboardSize(){
         return playboardSize;
@@ -18,5 +21,11 @@ private static int playboardSize=4;
     public static int getNrOfPlayers(){
         return 2;
     }
-
+    public static boolean addPlayer(Player player){
+        ar.add(player);
+        return true;
+    }
+    public static ArrayList<Player> getPlayers(){
+       return ar;
+    }
 }
