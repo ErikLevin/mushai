defb06ebbf0e88610a6e638439b1f3ac3caf6772
ErikAxelsson1 <ErikAxelsson1@850ccf3f-6c78-129b-0b64-280ab6fbb7b6>
2010-03-25 01:39:54 +0000

    have fix players, and comment away square pics

--- trunk/mushai/src/mushai/Window.java
+++ trunk/mushai/src/mushai/Window.java
@@ -5,7 +5,9 @@
 
 package mushai;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -19,15 +21,19 @@ public class Window extends JFrame {
     public Window(){
         super();
         setTitle("calender");
-        setPreferredSize(new Dimension(400,400));
-        setLayout(new GridLayout(1,2));
+        setPreferredSize(new Dimension(650,500));
+        setLayout(new FlowLayout());
         setResizable(false);
         setUndecorated(true);
-        JPanel playersPanel= new JPanel();
-        playersPanel.setLayout(new GridLayout(1,Settings.getNrOfPlayers()));
         Playboard pb=new Playboard();
-        
+        pb.setPreferredSize(new Dimension(500,  500));
         add(pb);
+        JPanel playersPanel= new JPanel();
+        playersPanel.setLayout(new GridLayout(Settings.getNrOfPlayers(),1));
+        playersPanel.setPreferredSize(new Dimension(120,500));
+        playersPanel.add(new Player("bark", Color.yellow));//fix temp
+        playersPanel.add(new Player("ai", Color.green));//fix temp
+        add(playersPanel);
         pack();
         this.setVisible(true);
     }
