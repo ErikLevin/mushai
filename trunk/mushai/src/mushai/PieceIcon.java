03caeb35a96d185c688f500185f71fc948257cce
minnenas.television <minnenas.television@850ccf3f-6c78-129b-0b64-280ab6fbb7b6>
2010-03-25 08:38:54 +0000

    Oops, forgot something :/

---
+++ trunk/mushai/src/mushai/PieceIcon.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mushai;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import javax.swing.Icon;
+
+/**
+ *
+ * @author Erik Levin
+ */
+public abstract class PieceIcon implements Icon {
+    protected Color color;
+
+    public abstract void paintIcon(Component c, Graphics g, int x, int y);
+}
