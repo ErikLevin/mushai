98dcfebfd0a226734a352c5ae9981456ad3dd5f3
ErikAxelsson1 <ErikAxelsson1@850ccf3f-6c78-129b-0b64-280ab6fbb7b6>
2010-04-19 08:30:11 +0000

    *split controller in model and controller

--- trunk/mushai/src/mushai/Controller.java
+++ trunk/mushai/src/mushai/Controller.java
@@ -16,12 +16,8 @@ public class Controller implements ActionListener {
 
     private Playboard board;
     private Window win;
-    HashSet<Point> posebleMoves = new HashSet<Point>();
-    /**
-     * The starting point for ongoing move
-     */
+
     private Point moveStart;
-    private ArrayList<Point> moves = new ArrayList<Point>();
 
     public Controller(Window tWin) {
         win = tWin;
@@ -41,10 +37,7 @@ public class Controller implements ActionListener {
 
 
         System.out.println(((JButton) e.getSource()).getText());
-        if (((JButton) e.getSource()).getText().equals("end turn")) {
-            // när du trycker på end turn kommer du från första i listan till sista
-            move(moves.get(0), moves.get(moves.size() - 1));
-        }
+
         if (e.getSource().getClass() == Tile.class) { //A Tile was clicked
             Tile t = (Tile) e.getSource();
             int xTile = 0, yTile = 0;
@@ -59,57 +52,52 @@ public class Controller implements ActionListener {
             }
 
             Point presedPoint = new Point(xTile, yTile);
+
+
             if (moveStart != null) {
-                if (whereCanIMove2(moveStart).contains(presedPoint)) {
-                    move(moveStart, presedPoint);
-                }
-            }
-            if (getPiece(presedPoint) != null) {
+                move(moveStart, presedPoint);
+
+            } else if (model.getPiece(presedPoint,board) != null) {
                 if (moveStart == null) {
-                    moveStart = presedPoint;
+
+                    for (int i = 0; i < Settings.getNrOfPlayers(); i++) {
+                        Player pl = Settings.getPlayers().get(i);
+                        if (pl.isItMyturn()) {
+
+                            if (model.getPiece(presedPoint,board).color.equals(pl.color)) {
+                                moveStart = presedPoint;
+                            }
+                        }
+                    }
+
                 }
             }
 
 
-            /*
-            System.out.println("x: " + xTile + " y: " + yTile);
-            if (t.getPiece() != null) { // is piece on tile
-            if (moves.isEmpty()) {
-            moves.add(new Point(xTile, yTile));
-            } else {
-            t.setBackground(Color.RED);
 
-            }
-            } else {
-            if (!moves.isEmpty()) {
-            canIMoveThere(moves.get(moves.size() - 1), new Point(xTile, yTile));
-            }
-            }
 
-            }
-
-             */        }
+        }
     }
 
-    private void move(Point start, Point end) {
-        Tile origin = board.getTiles()[start.x][start.y];
-        Piece p = origin.getPiece();
-        origin.setPiece(null);
-        board.getTiles()[end.x][end.y].setPiece(p);
-        moveStart = null;
-        changePlayer();
+    public void move(Point start, Point end) {
 
-        board.update();
+        if (model.whereCanIMove2(start,board).contains(end)) {
+            Tile origin = board.getTiles()[start.x][start.y];
+            Piece p = origin.getPiece();
+            origin.setPiece(null);
+            board.getTiles()[end.x][end.y].setPiece(p);
+            moveStart = null;
+            changePlayer();
+
+            board.update();
+        }
     }
 
-    public void changePlayer() {
+    private void changePlayer() {
 
 
         /**nollställer brädet **/
         ArrayList<Player> arL = Settings.getPlayers();
-        moves = new ArrayList<Point>();
-        posebleMoves=new HashSet<Point>();
-
         int xTile = 0, yTile = 0;
         for (xTile = 0; xTile < Settings.getPlayboardSize(); xTile++) {//målar om hela brädet
             for (yTile = 0; yTile < Settings.getPlayboardSize(); yTile++) {
@@ -133,192 +121,4 @@ public class Controller implements ActionListener {
         }
     }
 
-    private HashSet<Point> whereCanIMove2(Point from) {
-
-        Piece pi = getPiece(from);
-        for (Point diffPoint : pi.getMoves()) {
-            Point temp = new Point(from.x + diffPoint.x, from.y + diffPoint.y);
-            if (pointOnBorde(temp)) {
-
-
-                if (getPiece(temp) == null) {
-                    posebleMoves.add(temp);
-                }
-
-            }
-        }
-
-        posebleMoves.addAll(jumping(from, pi));
-        return posebleMoves;
-    }
-
-    private boolean pointOnBorde(Point point) {
-        if (point.x >= 0 && point.x < Settings.getPlayboardSize()) {
-            if (point.y >= 0 && point.y < Settings.getPlayboardSize()) {
-                return true;
-
-
-            }
-        }
-        return false;
-
-
-    }
-
-    private HashSet<Point> jumping(Point start, Piece pi) {
-
-
-
-        if (pointOnBorde(start)) {
-            posebleMoves.add(start);
-
-            for (Point diffPoint : pi.getMoves()) {
-                Point posOfPice = new Point(start.x + diffPoint.x, start.y + diffPoint.y);
-                if (pointOnBorde(posOfPice)) {
-
-                    if (getPiece(posOfPice) != null) {
-
-                        Point posAfterJump = new Point(start.x + diffPoint.x + diffPoint.x, start.y + diffPoint.y + diffPoint.y);
-
-                        if (pointOnBorde(posAfterJump)) {
-
-                            if (getPiece(posAfterJump) == null) {
-                               if(!posebleMoves.contains(posAfterJump)){
-                                posebleMoves.addAll(jumping(posAfterJump, pi));
-                               }
-
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return posebleMoves;
-
-
-    }
-
-    /* boolean canIMoveThere(Point from, Point to) {//ska bli ersatt med en bättre
-    System.out.println("from:" + from + "  to:" + to + "   size of moves:" + moves.size() + "bord size:" + board.getTiles().length);
-    Square pi = (Square) board.getTiles()[moves.get(0).x][moves.get(0).y].getPiece();
-    pi.updateMoves();
-
-    for (Point tempPoint : pi.getMoves()) {
-    boolean endTurn = true;
-
-    Point newPoint = new Point(from.x + tempPoint.x, from.y + tempPoint.y);
-    if (newPoint.x + tempPoint.x >= 0 && newPoint.x + tempPoint.x < Settings.getPlayboardSize()) {
-    if (newPoint.y + tempPoint.y >= 0 && newPoint.y + tempPoint.y < Settings.getPlayboardSize()) {
-
-    if (board.getTiles()[newPoint.x][newPoint.y].getPiece() != null) {
-    newPoint = new Point(newPoint.x + tempPoint.x, newPoint.y + tempPoint.y);
-    endTurn = false;
-    }
-    }
-    }
-    if (newPoint.equals(to)) {
-    if (moves.size() < 2 || !endTurn) {
-    moves.add(to);
-    return true;
-    }
-    if (endTurn) {
-    move(moves.get(0), moves.get(moves.size() - 1));
-    return true;
-    }
-    // drawLine mellan dessa rutor
-    }
-    }
-    return false;
-    }*//*
-    public ArrayList<Point> whereCanIMove(Point point) {
-    ArrayList<Point> legalMoves = new ArrayList<Point>();
-    legalMoves.add(point);
-    int xTile = 0, yTile = 0;
-    for (xTile = 0; xTile < Settings.getPlayboardSize(); xTile++) {//målar om hela brädet
-    for (yTile = 0; yTile < Settings.getPlayboardSize(); yTile++) {
-    if(whereCanIMove2(point))
-    if (canIMoveThere(point, new Point(xTile, yTile))) {
-    legalMoves.add(new Point(xTile, yTile));
-    }
-    }
-
-    }
-    return legalMoves;
-    }
-     */
-
-    public ArrayList<ArrayList<Point>> getallPosebleMoves() {
-        ArrayList<Point> pices = getYoursPieces();
-        ArrayList<ArrayList<Point>> allTheMoves = new ArrayList<ArrayList<Point>>();
-
-
-
-        for (Point p : pices) {
-            ArrayList<Point> temp = new ArrayList<Point>();
-            temp.add(p);
-
-
-            for (Point i : whereCanIMove2(p)) {
-                temp.add(i);
-
-
-            }
-            allTheMoves.add(temp);
-
-
-        }
-        return allTheMoves;
-
-
-    }
-
-    public ArrayList<Point> getYoursPieces() {
-        Color whichPlayerTurn = null;
-        ArrayList<Player> arL = Settings.getPlayers();
-
-
-        for (int i = 0; i
-                < arL.size(); i++) {
-            if (arL.get(i).isItMyturn()) {
-
-                whichPlayerTurn = arL.get(i).getColor();
-
-
-                break;
-
-
-            }
-        }
-
-        ArrayList<Point> pieces = new ArrayList<Point>();
-
-
-        int xTile = 0, yTile = 0;
-
-
-        for (xTile = 0; xTile
-                < Settings.getPlayboardSize(); xTile++) {
-            for (yTile = 0; yTile
-                    < Settings.getPlayboardSize(); yTile++) {
-                Piece tPiece = getPiece(new Point(xTile, yTile));
-
-
-                if (tPiece != null) {
-                    if (tPiece.color == whichPlayerTurn) {
-                        pieces.add(new Point(xTile, yTile));
-
-
-                    }
-                }
-            }
-        }
-        return pieces;
-
-
-    }
-
-    private Piece getPiece(Point point) {
-        return board.getTiles()[point.x][point.y].getPiece();
-
-    }
 }
---
+++ trunk/mushai/src/mushai/model.java
@@ -0,0 +1,180 @@
+package mushai;
+
+
+import java.awt.Color;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.HashSet;
+import mushai.Piece;
+import mushai.Player;
+import mushai.Settings;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bark
+ */
+public class model {
+
+    private static HashSet<Point> posebleMoves = new HashSet<Point>();
+/*
+    public static boolean canIGo(Point to,Point from, ArrayList[][] bord){
+        
+        
+    }
+
+    public static ArrayList<ArrayList<Point>> getAllMoves(){
+
+
+    }
+
+*/
+
+
+    public static HashSet<Point> whereCanIMove2(Point from,Playboard board) {
+        posebleMoves = new HashSet<Point>();//nollar listan
+        Piece pi = getPiece(from,board);
+        for (Point diffPoint : pi.getMoves()) {
+            Point temp = new Point(from.x + diffPoint.x, from.y + diffPoint.y);
+            if (pointOnBorde(temp)) {
+
+
+                if (getPiece(temp,board) == null) {
+                    posebleMoves.add(temp);
+                }
+
+            }
+        }
+
+        posebleMoves.addAll(jumping(from, pi,board));
+        posebleMoves.remove(from);
+        return posebleMoves;
+    }
+
+    private static boolean pointOnBorde(Point point) {
+        if (point.x >= 0 && point.x < Settings.getPlayboardSize()) {
+            if (point.y >= 0 && point.y < Settings.getPlayboardSize()) {
+                return true;
+
+
+            }
+        }
+        return false;
+
+
+    }
+
+    private static HashSet<Point> jumping(Point start, Piece pi,Playboard board) {
+
+
+
+        if (pointOnBorde(start)) {
+            posebleMoves.add(start);
+
+            for (Point diffPoint : pi.getMoves()) {
+                Point posOfPice = new Point(start.x + diffPoint.x, start.y + diffPoint.y);
+                if (pointOnBorde(posOfPice)) {
+
+                    if (getPiece(posOfPice,board) != null) {
+
+                        Point posAfterJump = new Point(start.x + diffPoint.x + diffPoint.x, start.y + diffPoint.y + diffPoint.y);
+
+                        if (pointOnBorde(posAfterJump)) {
+
+                            if (getPiece(posAfterJump,board) == null) {
+                                if (!posebleMoves.contains(posAfterJump)) {
+                                    posebleMoves.addAll(jumping(posAfterJump, pi,board));
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return posebleMoves;
+
+
+    }
+
+    public ArrayList<ArrayList<Point>> getallPosebleMoves(Playboard board) {
+        ArrayList<Point> pices = getYoursPieces(board);
+        ArrayList<ArrayList<Point>> allTheMoves = new ArrayList<ArrayList<Point>>();
+
+
+
+        for (Point p : pices) {
+            ArrayList<Point> temp = new ArrayList<Point>();
+            temp.add(p);
+
+
+            for (Point i : whereCanIMove2(p,board)) {
+                temp.add(i);
+
+
+            }
+            allTheMoves.add(temp);
+
+
+        }
+        return allTheMoves;
+
+
+    }
+
+    public static ArrayList<Point> getYoursPieces(Playboard board) {
+        Color whichPlayerTurn = null;
+        ArrayList<Player> arL = Settings.getPlayers();
+
+
+        for (int i = 0; i
+                < arL.size(); i++) {
+            if (arL.get(i).isItMyturn()) {
+
+                whichPlayerTurn = arL.get(i).getColor();
+
+
+                break;
+
+
+            }
+        }
+
+        ArrayList<Point> pieces = new ArrayList<Point>();
+
+
+        int xTile = 0, yTile = 0;
+
+
+        for (xTile = 0; xTile
+                < Settings.getPlayboardSize(); xTile++) {
+            for (yTile = 0; yTile
+                    < Settings.getPlayboardSize(); yTile++) {
+                Piece tPiece = getPiece(new Point(xTile, yTile),board);
+
+
+                if (tPiece != null) {
+                    if (tPiece.color == whichPlayerTurn) {
+                        pieces.add(new Point(xTile, yTile));
+
+
+                    }
+                }
+            }
+        }
+        return pieces;
+
+
+    }
+
+    public static Piece getPiece(Point point,Playboard board) {
+        return board.getTiles()[point.x][point.y].getPiece();
+
+    }
+
+
+}
