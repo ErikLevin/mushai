496ec455b627ab4f58b987a54480a74decebdfa7
darukusu <darukusu@850ccf3f-6c78-129b-0b64-280ab6fbb7b6>
2010-05-07 14:38:01 +0000

    circle added

--- mushai/src/mushai/Controller.java
+++ mushai/src/mushai/Controller.java
@@ -152,7 +152,7 @@ public class Controller implements ActionListener {
                 thisPlayer.setMyTurn(true);
                 switch (thisPlayer.getType()) {
                     case MINIMAX:
-                        move(minimax.findBestMove(5));
+                        move(minimax.findBestMove(4));
                         break;
                     case GENETIC:
                         thisPlayer.makeMove(board);
