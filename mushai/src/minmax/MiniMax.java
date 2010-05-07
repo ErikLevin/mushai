d57b0d0cfa3016719a4ecd5ecbebefc4a7ffca3e
darukusu <darukusu@850ccf3f-6c78-129b-0b64-280ab6fbb7b6>
2010-05-07 11:16:45 +0000

    trying to fix things

--- mushai/src/minmax/MiniMax.java
+++ mushai/src/minmax/MiniMax.java
@@ -36,6 +36,7 @@ public class MiniMax {
         playboard = new PlayboardModel(originalPlayboard, turn);
         MoveAndFitness maf = findBestMove(depth, null);
         Move move = maf.move;
+        //Move move = breadthFirst(depth);
         ArrayList<Player> arL = Settings.getPlayers();
         if (turn == 0) {
             arL.get(0).setMyTurn(true);
@@ -89,6 +90,10 @@ public class MiniMax {
             if (maf.fitness > 1000 || maf.fitness == 0) {
                 //return new MoveAndFitness(move, maf.fitness);
             }
+            if (value == 0 || value == 10000) {
+                value += depth;
+                maf.fitness += depth;
+            }
             if (turn == 0) {
                 if (value > bestValue) {
                     //if (lastMove!=null)System.out.println("LAST: "+lastMove.getStart() + " ---> " + lastMove.getEnd() + ":::" + Model.getBoardFitness(playboard));
@@ -107,117 +112,6 @@ public class MiniMax {
         return new MoveAndFitness(bestMove, bestValue);
     }
 
-    /*private MoveAndFitness findBestMovebyBreadth(int depth, Move lastMove) {
-    if (depth == 0 || Model.getBoardFitness(playboard) > 1000 || Model.getBoardFitness(playboard) == 0) {
-    try {
-    return new MoveAndFitness(lastMove, Model.getBoardFitness(playboard));
-    } catch (Exception ex) {
-    Logger.getLogger(MiniMax.class.getName()).log(Level.SEVERE, null, ex);
-    }
-    }
-    int bestValue, value, turn;
-    Move bestMove;
-    turn = Model.whoseTurnIsIt();
-    List<Move> possibleMoves = Model.getAllPossibleMoves(playboard);
-    if (possibleMoves.isEmpty()) {
-    throw new RuntimeException("CRITICAL ERROR EXCEPTION! NO POSSIBLE MOVES :(((((");
-    }
-
-    LinkedList<Move> queue = new LinkedList<Move>();
-    for (Move move : possibleMoves){
-    queue.add(move);
-    }
-    Move nextMove = queue.removeFirst();
-    controller.move(nextMove);
-
-
-
-    Move firstMove = possibleMoves.remove(0);
-    controller.move(firstMove);
-    //if (lastMove!=null)System.out.println("LAST: "+lastMove.getStart() + " ---> " + lastMove.getEnd() + ":::" + Model.getBoardFitness(playboard));
-    //System.out.println(firstMove.getStart() + " ---> " + firstMove.getEnd() + ":::" + Model.getBoardFitness(playboard));
-    MoveAndFitness maf = findBestMovebyBreadth(depth - 1, firstMove);
-    bestValue = maf.fitness;
-    Model.movePiece(playboard, firstMove.getEnd(), firstMove.getStart());
-    controller.changePlayer();
-    if (maf.fitness > 1000 || maf.fitness == 0) {
-    //return new MoveAndFitness(firstMove, maf.fitness);
-    }
-    bestMove = firstMove;
-    for (Move move : possibleMoves) {
-    //if (depth == 4) System.out.println(move.getStart() + "--->"+ move.getEnd());
-    //domove
-    controller.move(move);
-    maf = findBestMovebyBreadth(depth - 1, move);
-    value = maf.fitness;
-    //undomove
-    Model.movePiece(playboard, move.getEnd(), move.getStart());
-    controller.changePlayer();
-    if (maf.fitness > 1000 || maf.fitness == 0) {
-    //return new MoveAndFitness(move, maf.fitness);
-    }
-    if (turn == 0) {
-    if (value > bestValue) {
-    //if (lastMove!=null)System.out.println("LAST: "+lastMove.getStart() + " ---> " + lastMove.getEnd() + ":::" + Model.getBoardFitness(playboard));
-    //System.out.println(move.getStart() + " ---> " + move.getEnd() + ":::" + Model.getBoardFitness(playboard));
-    bestValue = value;
-    bestMove = move;
-    //System.out.println("depth: " + depth + " value: " + bestValue);
-    }
-    } else if (turn == 1) {
-    if (value < bestValue) {
-    bestValue = value;
-    bestMove = move;
-    }
-    }
-    }
-    return new MoveAndFitness(bestMove, bestValue);
-    }
-    private MoveAndFitness negaMax(int depth, Move lastMove) {
-    if (depth == 0 || Model.getBoardFitness(playboard) > 1000 || Model.getBoardFitness(playboard) == 0) {
-    //System.out.println(Model.getBoardFitness(playboard));
-    try {
-    return new MoveAndFitness(lastMove, Model.getBoardFitness(playboard));
-    } catch (Exception ex) {
-    Logger.getLogger(MiniMax.class.getName()).log(Level.SEVERE, null, ex);
-    }
-    }
-    int bestValue, value, turn;
-    Move bestMove;
-    turn = Model.whoseTurnIsIt();
-    List<Move> possibleMoves = Model.getAllPossibleMoves(playboard);
-    if (possibleMoves.isEmpty()) {
-    throw new RuntimeException("CRITICAL ERROR EXCEPTION! NO POSSIBLE MOVES :(((((");
-    }
-    bestValue = -1337;
-    bestMove = null;
-    MoveAndFitness maf;
-
-    for (Move move : possibleMoves) {
-    //domove
-    controller.move(move);
-
-    maf = negaMax(depth - 1, move);
-    value = maf.fitness;
-    if (turn == 1) {
-    value = -value;
-    }
-    //undomove
-    Model.movePiece(playboard, move.getEnd(), move.getStart());
-    controller.changePlayer();
-    System.out.println("value: " + value + " bestValue: " + bestValue);
-    if (value > bestValue) {
-    bestValue = value;
-    bestMove = move;
-    }
-    }
-    if (bestMove == null) {
-    System.out.println("OOOOOOOH NOOOOOOOOOOOOO!!!!!!!");
-    }
-    System.out.println(possibleMoves.get(0));
-    System.out.println(bestValue);
-    return new MoveAndFitness(lastMove, bestValue);
-    }*/
     /*
     currDepth = 0
     for(Move m : getAllPossibleMoves)
@@ -243,61 +137,107 @@ public class MiniMax {
         int currDepth = 0;
         int fitness = playboard.getBoardFitness();
         int bestFitness = 0;
+        if (playboard.getTurn() == 1) {
+            bestFitness = 0;
+        }
         LinkedList<MoveAndPrevMoves> queue = new LinkedList<MoveAndPrevMoves>();
         for (Move m : playboard.getAllPossibleMoves(playboard.getTurn())) {
-            queue.push(new MoveAndPrevMoves(m,new LinkedList<Move>()));
+            queue.add(new MoveAndPrevMoves(m, new LinkedList<Move>()));
         }
         Move move, bestMove, temp;
         bestMove = null;
-        LinkedList<Move> prevMoves;
+        LinkedList<Move> prevMoves = new LinkedList<Move>();
+        LinkedList<Move> prevClone = new LinkedList<Move>();
         while (currDepth <= depth) {
+            /*System.out.println("queue:");
+            for (MoveAndPrevMoves ma : queue){
+            System.out.println("\tmove: " + ma.move + " prevMoves: ");
+            for (Move prev : ma.prevMoves)
+            System.out.println("\t\t" + prev);
+            }*/
+            if (queue.isEmpty()) {
+                System.out.println("reached empty, maybe correct");
+                System.out.println(currDepth);
+                break;
+            }
             MoveAndPrevMoves mapm = queue.pop();
             move = mapm.move;
+
             prevMoves = mapm.prevMoves;
             currDepth = prevMoves.size();
+            //System.out.println(currDepth + " " + move);
             //doallmoves
-            for (Move m : prevMoves)
+            for (Move m : prevMoves) {
                 playboard.movePiece(m.getStart(), m.getEnd());
+            }
             playboard.movePiece(move.getStart(), move.getEnd());
 
             fitness = playboard.getBoardFitness();
+            System.out.println("turn: " + playboard.getTurn() + ", move made: " + move);
+            System.out.println("fitness: " + fitness + ", bestfitness: " + bestFitness);
+            if (playboard.getTurn() == 1) {
+                if (fitness > bestFitness) {
+                    bestFitness = fitness;
+                    if (prevMoves.isEmpty()) {
+                        bestMove = move;
+                    } else {
+                        bestMove = prevMoves.getFirst();
+                    }
+                }
+            } else if (playboard.getTurn() == 0) {
+                if (fitness < bestFitness) {
+                    bestFitness = fitness;
+                    if (prevMoves.isEmpty()) {
+                        bestMove = move;
+                    } else {
+                        bestMove = prevMoves.getFirst();
+                    }
+                }
+            }
+            System.out.println("bestMove right now: " + bestMove);
+            System.out.println("after check, fitness: " + fitness + ", bestfitness: " + bestFitness);
             if (playboard.checkWin() != 0) {
+                System.out.println("possible win! should break! " + currDepth + " " + bestMove);
+                for (Move m : prevMoves) {
+                    System.out.println(m);
+                }
                 break;
             }
-            if (playboard.getTurn() == 0){
-                if (fitness > bestFitness){
-                    bestFitness = fitness;
-                    bestMove = prevMoves.getFirst();
-                }
-            }
-            else if (playboard.getTurn() == 1){
-                if (fitness < bestFitness){
-                    bestFitness = fitness;
-                    bestMove = prevMoves.getFirst();
-                }
-            }
 
+            prevClone = new LinkedList<Move>();
+            for (Move m : prevMoves) {
+                prevClone.add(m);
+            }
+            prevClone.add(move);
             if (currDepth < depth) {
                 for (Move m : playboard.getAllPossibleMoves(playboard.getTurn())) {
-                    queue.push(new MoveAndPrevMoves(m,prevMoves));
+                    queue.add(new MoveAndPrevMoves(m, prevClone));
                 }
             }
             playboard.movePiece(move.getEnd(), move.getStart());
-            for (int i = prevMoves.size()-1; i >= 0; i--){
+            for (int i = prevMoves.size() - 1; i >= 0; i--) {
                 temp = prevMoves.get(i);
                 playboard.movePiece(temp.getEnd(), temp.getStart());
             }
         }
+        if (bestMove == null) {
+            System.out.println("RETURNING NULL AS BEST MOVE!");
+        }
+        System.out.println("move chosen: " + bestMove);
         return bestMove;
     }
-    private class MoveAndPrevMoves{
+
+    private class MoveAndPrevMoves {
+
         Move move;
         LinkedList<Move> prevMoves;
-        public MoveAndPrevMoves(Move m, LinkedList<Move> pm){
+
+        public MoveAndPrevMoves(Move m, LinkedList<Move> pm) {
             prevMoves = pm;
             move = m;
         }
     }
+
     private class MoveAndFitness {
 
         Move move;
