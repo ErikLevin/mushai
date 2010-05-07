95a43deced8a7ca120e3f9c0f4bba201f92131fd
minnenas.television <minnenas.television@850ccf3f-6c78-129b-0b64-280ab6fbb7b6>
2010-05-07 18:39:13 +0000

    Fixing a few bugs and tinkering with Setingswindow

--- mushai/src/mushai/Main.java
+++ mushai/src/mushai/Main.java
@@ -10,5 +10,24 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("starting program");
         SettingsWindow setWindow = new SettingsWindow();
+
+
+         Settings.nullPlayers();
+        Player player1 = new Player("You", Color.yellow);
+        Settings.addPlayer(player1);
+
+        Player player2 = new Player("Ai", Color.cyan);
+        player2.setMyTurn(true);
+        player2.setType(Player.PlayerType.MINIMAX);
+
+        Settings.addPlayer(player2);
+
+
+
+        Settings.setPlayboardSize(8);
+
+        Window win = new Window();
+        Controller controller = new Controller(win);
+
     }
 }
