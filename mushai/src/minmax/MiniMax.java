07bdf6fd8c25ffa231e945cc834cf6517fbff395
darukusu <darukusu@850ccf3f-6c78-129b-0b64-280ab6fbb7b6>
2010-05-13 21:09:25 +0000

    commented findBestMove

--- mushai/src/minmax/MiniMax.java
+++ mushai/src/minmax/MiniMax.java
@@ -40,25 +40,36 @@ public class MiniMax {
         return move;
     }
 
+    //Depth first traversing through the possible moves and their submoves
+    //to find the best move.
     private MoveAndFitness findBestMove(int depth, Move lastMove) {
-        if (lastMove != null) //System.out.println("after move: " + lastMove + " fitness is: " + playboard.getUtility());
-        {
-            if (depth == 0 || playboard.getUtility() > 1000 || playboard.getUtility() < -1000) {
-                try {
-                    return new MoveAndFitness(lastMove, playboard.getUtility());
-                } catch (Exception ex) {
-                    Logger.getLogger(MiniMax.class.getName()).log(Level.SEVERE, null, ex);
-                }
+        //if we have checked far enough down depth-wise (reached a leaf) we return
+        //with the current fitness-value. It also checks and returns if somebody has won.
+        //Since the endgame is handled elsewhere, maybe we should only need check depth here
+        //Only reason to handle it is if it actually has a large enough depth to investigate
+        //from before endGame hits and until someone actually wins. Highly unlikely.
+        //Please give supercomputer
+        if (depth == 0 || playboard.getUtility() > 1000 || playboard.getUtility() < -1000) {
+            try {
+                return new MoveAndFitness(lastMove, playboard.getUtility());
+            } catch (Exception ex) {
+                Logger.getLogger(MiniMax.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+
+        //initiation of needed variables
         int bestValue, value, turn;
         Move bestMove;
         turn = Model.whoseTurnIsIt();
+
         List<Move> possibleMoves = playboard.getAllPossibleMoves(turn);
         int noOfPossibleMoves = possibleMoves.size();
         if (possibleMoves.isEmpty()) {
             throw new RuntimeException("CRITICAL ERROR EXCEPTION! NO POSSIBLE MOVES :(((((");
         }
+
+        //Taking out first move from possible moves and set it as bestMove
+        //so there is something to compare the rest of the moves with
 
         Move firstMove = possibleMoves.remove(0);
         playboard.movePiece(firstMove.getStart(), firstMove.getEnd());
@@ -66,16 +77,29 @@ public class MiniMax {
         bestValue = maf.fitness;
         playboard.movePiece(firstMove.getEnd(), firstMove.getStart());
         bestMove = firstMove;
+
+        //Is this random really necessary? None of the games seems to be random
+        //when pitching two minimax-ai:s against each other
         double randomValue;
         Random randomizer = new Random();
+
+        //Search through all possible moves to find the best move
+        //Every move is made and then all possible submoves are checked until
+        //depth has reached 0. After return of the best submoves, the value is
+        //saved, the move undone and the move is compared to the bestMove
         for (Move move : possibleMoves) {
-            //if (depth == 4) System.out.println(move.getStart() + "--->"+ move.getEnd());
             //domove
             playboard.movePiece(move.getStart(), move.getEnd());
+
+            //find best submove for this move
             maf = findBestMove(depth - 1, move);
             value = maf.fitness;
             //undomove
             playboard.movePiece(move.getEnd(), move.getStart());
+
+            //adding depth probably no longer necessary because endgame is not handled here
+            //If we ever reached a state where we could check a large enough depth,
+            //this would be useful. But that is higly unlikely.
             if (value == -10000 || value == 10000) {
                 if (turn == 0) {
                     value += depth;
@@ -85,7 +109,12 @@ public class MiniMax {
                     maf.fitness -= depth;
                 }
             }
+            //Not necessary? See earlier comments about random
             randomValue = randomizer.nextDouble();
+
+            //depending on whos turn it is, a higher/lower fitness is wanted
+            //question is if it should be changed to > and < instead of >= and <=
+            //and removing of the randomcheck altogether. See earlier comments about that
             if (turn == 0) {
                 if (value >= bestValue) {
                     if (!(value == bestValue && randomValue > (1 / noOfPossibleMoves))) {
@@ -102,6 +131,7 @@ public class MiniMax {
                 }
             }
         }
+        //This place should really not be reached... Ever... But it is possible..
         return new MoveAndFitness(bestMove, bestValue);
     }  
 
@@ -123,6 +153,9 @@ public class MiniMax {
         }
     }
 
+    //When endGameSituation hits, this will be used instead of findBestMove.
+    //This goes breadth first and goes through all possible moves regardless of
+    //moves in earlier depth. It ignores the opponents moves completely.
     private Move findEndGameMove(int depth) {
         int currDepth = 0;
         int fitness = playboard.getUtility();
