473444350c9a0aec04e312c19d5ecbe7e48480b3
darukusu <darukusu@850ccf3f-6c78-129b-0b64-280ab6fbb7b6>
2010-05-13 11:12:06 +0000

    ...

--- mushai/src/mushai/Main.java
+++ mushai/src/mushai/Main.java
@@ -21,12 +21,12 @@ public class Main {
 
         Settings.nullPlayers();
         Player player1 = new Player("You", Color.yellow);
-//        player1.setType(Player.PlayerType.MINIMAX);
+        player1.setType(Player.PlayerType.MINIMAX);
         Settings.addPlayer(player1);
 
         Player player2 = new Player("Ai", Color.cyan);
         player2.setMyTurn(true);
-//        player2.setType(Player.PlayerType.MINIMAX);
+        player2.setType(Player.PlayerType.MINIMAX);
         //player2.setType(Player.PlayerType.GENETIC);
 
         Settings.addPlayer(player2);
